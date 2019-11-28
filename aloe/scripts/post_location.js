ba4369a6c583cf3b1b5f46b7af6e8421700198b6
ShawkBawks <lroberts77@gmail.com>
2019-11-28 00:24:37 +0000

    Push before attempting to set up sensor reading data

--- aloe/scripts/post_location.js
+++ aloe/scripts/post_location.js
@@ -1,16 +1,27 @@
-var express = require('require');
+var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 require('dotenv').config();
 const key = process.env.ACCESS_KEY;
-axios.get(`http://api.istack.com/check?access_key=${key}&fields=latitude,longitude`)
-	.then function (response){
+
+const url = `http://api.istack.com/check?access_key=${key}&fields=latitude,longitude`;
+console.log('url', url);
+axios.get('https://google.com')
+.then(res => {
+	console.log('res', res);
+}).catch(e => console.log(e));
+axios.get(url)
+	.then(function (response){
+		console.log('first then:')
 		axios({
 			method: 'post',
-			url: 'api/user-sensors',
+			url: 'http://localhost:3001/api/new-user-sensor',
 			data: {
-				latitude: response.data.latitude,
-				longitude: response.data.longitude
+				longitude: response.data.longitude,
+				latitude: response.data.latitude
 			}
-		})
-	};
+		}).then(console.log('last then'))
+	}).catch(e => {
+		console.log('error!', e);
+	})
+//refactor cb:
