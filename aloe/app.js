2fd75f7f587be12545aed34c290663d031cb1e26
ShawkBawks <lroberts77@gmail.com>
2019-11-29 19:51:04 -0800

    importing sensorControl into app.js

--- aloe/app.js
+++ aloe/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const request = require('request');
 const axios = require("axios");
+const sensorControl = require("./sensorControl")
 require("dotenv").config();
 var app = express();
 
